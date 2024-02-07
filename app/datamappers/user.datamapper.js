4a22dc199c13078c83dec4b13d2aad5718413498
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-07 18:54:36 +0100

    add user datamapper

---
+++ app/datamappers/user.datamapper.js
@@ -0,0 +1,38 @@
+/**
+ * @typedef {object} User
+ * @property {integer} id - User id
+ * @property {string} email - User email
+ * @property {string} password - User password
+ * @property {string} firstname - User firstname
+ * @property {string} lastname - User lastname
+ * @property {date} birthdate - User birthdate
+ * @property {string} address - User address
+ * @property {string} zipcode - User zipcode
+ * @property {string} city - User city
+ * @property {integer} country - User country
+ * @property {string} role - User role
+ * @property {string} created_at - User created_at
+ * @property {string} updated_at - User updated_at
+ */
+
+/**
+ * @typedef {object} UserInput
+ * @property {string} email - User email
+ * @property {string} password - User password
+ * @property {string} firstname - User firstname
+ * @property {string} lastname - User lastname
+ * @property {date} birthdate - User birthdate
+ * @property {string} address - User address
+ * @property {string} zipcode - User zipcode
+ * @property {string} city - User city
+ * @property {integer} country - User country
+ * @property {string} role - User role
+ */
+
+import CoreDatamapper from './core.datamapper.js';
+
+export default class UserDatamapper extends CoreDatamapper {
+  static readTableName = 'user';
+
+  static writeTableName = 'user';
+}
