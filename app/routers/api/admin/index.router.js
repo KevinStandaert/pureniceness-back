05692f81504e43a13fd07fde49c0de5694782744
kevinstandaert <kevin.standaert@oclock.school>
2024-02-08 15:22:32 +0100

    feat index.router for admin + refactoring routers api folder

---
+++ app/routers/api/admin/index.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import ApiError from '../../../errors/api.error.js';
+import apiAdminUserRouter from './admin.user.router.js';
+import apiAdminLabelRouter from './admin.label.router.js';
+
+const apiAdminRouter = express.Router();
+
+apiAdminRouter.use('/admin/users', apiAdminUserRouter);
+
+apiAdminRouter.use('/admin/labels', apiAdminLabelRouter);
+
+apiAdminRouter.use((_, __, next) => {
+  next(new ApiError('Resource not found', { httpStatus: 404 }));
+});
+
+export default apiAdminRouter;
--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import apiRouter from './api/index.router.js';
+import apiRouter from './api/site/index.router.js';
+import apiAdminRouter from './api/admin/index.router.js';
 
 const router = express.Router();
 
-router.use('/api', apiRouter);
+router.use('/api', apiRouter, apiAdminRouter);
 
 export default router;
