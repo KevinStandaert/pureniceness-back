5f79c392079c3e3a5e48fcab5c3af6df7967c494
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-08 18:05:46 +0100

    fix(label.create.schema) fix error messages

--- app/schemas/label.create.shema.js
+++ app/schemas/label.create.shema.js
@@ -1,12 +1,19 @@
 import Joi from 'joi';
 
 const createLabelSchema = Joi.object({
-  name: Joi.string().trim().required().error(new Error('Le nom du label est obligatoire')),
-  year: Joi.integer().min(4).max(4).required().error(new Error('L\'année de création du label doit être sous la forme YYYY')),
-  city: Joi.string().trim().required().error(new Error('Le nom de la ville est obligatoire')),
-  country: Joi.string().trim().required().error(new Error('Le nom du pays est obligatoire')),
-  description: Joi.string().required().error(new Error('Une description du label est obligatoire')),
-  url_image: Joi.string().trim().required().error(new Error('L\'URL de l\'image est obligatoire')),
+  name: Joi.string().required()
+    .error(new Error('Le nom du label est requis et doit être une chaîne de caractères')),
+  year: Joi.number().integer().min(1000).max(9999)
+    .required()
+    .error(new Error('L\'année de création du label doit être au format YYYY')),
+  city: Joi.string().required()
+    .error(new Error('Le nom de la ville est requis et doit être une chaîne de caractères')),
+  country: Joi.string().required()
+    .error(new Error('Le nom du pays est requis et doit être une chaîne de caractères')),
+  description: Joi.string().required()
+    .error(new Error('Une description du label est requise et doit être une chaîne de caractères')),
+  url_image: Joi.string().required()
+    .error(new Error('L\'URL de l\'image est requise et doit être une url')),
 });
 
 export default createLabelSchema;
