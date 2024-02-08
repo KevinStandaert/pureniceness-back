f949c927c0dee7af9d86ed663ddd3c9064b711e3
kevinstandaert <kevin.standaert@oclock.school>
2024-02-08 16:27:04 +0100

    feat admin.social.router

--- app/routers/api/admin/index.router.js
+++ app/routers/api/admin/index.router.js
@@ -3,6 +3,7 @@ import ApiError from '../../../errors/api.error.js';
 import apiAdminUserRouter from './admin.user.router.js';
 import apiAdminLabelRouter from './admin.label.router.js';
 import apiAdminArtistRouter from './admin.artist.router.js';
+import apiAdminSocialRouter from './admin.social.router.js';
 
 const apiAdminRouter = express.Router();
 
@@ -11,6 +12,8 @@ apiAdminRouter.use('/users', apiAdminUserRouter);
 apiAdminRouter.use('/labels', apiAdminLabelRouter);
 
 apiAdminRouter.use('/artists', apiAdminArtistRouter);
+
+apiAdminRouter.use('/socials', apiAdminSocialRouter);
 
 apiAdminRouter.use((_, __, next) => {
   next(new ApiError('Resource not found', { httpStatus: 404 }));
