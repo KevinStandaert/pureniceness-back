4e85d4df317e89c25f73cc8a568569d020f24560
kevinstandaert <kevin.standaert@oclock.school>
2024-02-08 11:59:20 +0100

    feat track.datamapper

---
+++ app/datamappers/track.datamapper.js
@@ -0,0 +1,7 @@
+import CoreDatamapper from './core.datamapper.js';
+
+export default class TrackDatamapper extends CoreDatamapper {
+  static readTableName = 'track';
+
+  static writeTableName = 'track';
+}
