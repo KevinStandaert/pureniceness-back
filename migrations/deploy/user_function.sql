c870937c9d0cc32500834985598deb81442ef302
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-08 11:32:02 +0100

    feat(user_function.sql): add create_user, update_user SQL functions migration

---
+++ migrations/deploy/user_function.sql
@@ -0,0 +1,51 @@
+-- Deploy pureniceness:user_function to pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION "create_user"(data json) RETURNS "user" AS $$
+
+  INSERT INTO "user"
+  (
+    "email", 
+    "password",
+    "firstname",
+    "lastname",
+    "birthdate",
+    "address", 
+    "zipcode", 
+    "city",
+    "country"
+  ) VALUES (
+    data->>'email',
+    data->>'password',
+    data->>'firstname',
+    data->>'lastname',
+    (data->>'birthdate')::date,
+    data->>'address',
+    data->>'zipcode',
+    data->>'city',
+    data->>'country'
+  )
+  RETURNING *;
+
+$$ LANGUAGE sql STRICT;
+
+CREATE OR REPLACE FUNCTION "update_user"(data json) RETURNS "user" AS $$
+
+  UPDATE "user" SET
+    "email" = COALESCE(data->>'email', "email"),
+    "password" = COALESCE(data->>'password', "password"),
+    "firstname" = COALESCE(data->>'firstname', "firstname"),
+    "lastname" = COALESCE(data->>'lastname', "lastname"),
+    "birthdate" = COALESCE((data->>'birthdate')::date, "birthdate"),
+    "address" = COALESCE(data->>'address', "address"),
+    "zipcode" = COALESCE(data->>'zipcode', "zipcode"),
+    "city" = COALESCE(data->>'city', "city"),
+    "country" = COALESCE(data->>'country', "country"),
+    "updated_at" = now()
+  WHERE id = (data->>'id')::int
+  RETURNING *;
+
+$$ LANGUAGE sql STRICT;
+
+COMMIT;
