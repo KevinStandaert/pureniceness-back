136f4a3295e7ef1765a3f653937d58417190cf7d
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-08 19:27:45 +0100

    fix(index.router): add missing auth router

--- app/routers/api/index.router.js
+++ app/routers/api/index.router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiRouter from './site/index.router.js';
 import adminRouter from './admin/index.router.js';
+import authRouter from './site/auth.router.js';
 import errorHandler from '../../helpers/error.handler.js';
 
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 router.use('/site', apiRouter);
 
 router.use('/admin', adminRouter);
+
+router.use('/auth', authRouter);
 
 router.use(errorHandler);
 
