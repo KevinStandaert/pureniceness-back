ce9db5a3cb2dd5cf06191fdd8b2bb458aead6f52
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-08 18:06:26 +0100

    feat(event.create.schema) add create event schema

---
+++ app/schemas/event.create.schema.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const createEventSchema = Joi.object({
+  name: Joi.string().required()
+    .error(new Error('Le nom de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  type: Joi.string().required()
+    .error(new Error('Le type de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  description: Joi.string().required()
+    .error(new Error('La description de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  starting_date: Joi.date().iso().required()
+    .error(new Error('La date de début de l\'événement est obligatoire et doit être au format ISO')),
+  ending_date: Joi.date().iso().required().greater(Joi.ref('starting_date'))
+    .error(new Error('La date de fin de l\'événement doit être postérieure à la date de début et doit être au format ISO')),
+  city: Joi.string().required()
+    .error(new Error('La ville de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  country: Joi.string().required()
+    .error(new Error('Le pays de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  location: Joi.string().required()
+    .error(new Error('L\'emplacement de l\'événement est obligatoire et doit être une chaîne de caractères')),
+  url_image: Joi.string().uri().required()
+    .error(new Error('L\'URL de l\'image de l\'événement est obligatoire et doit être une URL valide')),
+});
+
+export default createEventSchema;
