7f7e0b3f8a3ff2a4ae50c218de2692f72f983e37
CedricJardon <yopla666@gmail.com>
2024-02-08 16:43:45 +0100

    feature(auth.controller)added getSignout getSignin and getSignup

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,18 +1,73 @@
+import bcrypt from 'bcrypt';
 import userDatamapper from '../datamappers/user.datamapper.js';
+import ApiError from '../errors/api.error.js';
+import userController from './user.controller.js';
 
 export default class AuthController {
   static datamapper = userDatamapper;
 
-  static async getSignout(_, res) {
-    console.log('Deconnexion');
-    res.status(200).json('Déconnexion réussie');
+  static async getSignout(req, res) {
+    return res.status(200).json('Déconnexion réussie');
   }
 
-  static async postSignin(req, res) {
-    res.status(200).json('Connexion réussie');
+  static async postSignin(req, res, next) {
+    const { email, password } = req.body;
+    const existingUser = await userDatamapper.findByEmail(email);
+
+    if (!existingUser) {
+      const err = new ApiError(
+        'Mot de passe ou email incorrect.',
+        { httpStatus: 401 },
+      );
+      return next(err);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const isValidPassword = bcrypt.compare(hashedPassword, existingUser.password);
+
+    if (!isValidPassword) {
+      const err = new ApiError(
+        'Mot de passe ou email incorrect.',
+        { httpStatus: 401 },
+      );
+      return next(err);
+    }
+
+    return res.status(200).json('Connexion réussie');
   }
 
-  static async postSignup(req, res) {
-    res.status(200).json('Inscription réussie');
+  static async postSignup(req, res, next) {
+    const { password, ...bodyWithoutPassword } = req.body;
+
+    const existingUser = await userDatamapper.findByEmail(bodyWithoutPassword.email);
+
+    if (existingUser) {
+      const err = new ApiError(
+        'Email déjà utilisé.',
+        { httpStatus: 409 },
+      );
+      return next(err);
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userData = {
+      body: {
+        ...bodyWithoutPassword,
+        password: hashedPassword,
+      },
+    };
+
+    const userCreated = await userController.create(userData);
+    console.log(userCreated);
+    if (!userCreated) {
+      const err = new ApiError(
+        'Erreur lors de la création du compte.',
+        { httpStatus: 400 },
+      );
+      return next(err);
+    }
+
+    return res.status(201).json('Inscription réussie');
   }
 }
