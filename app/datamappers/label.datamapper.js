cc6398b8e5be8dce9db1c275d4f3d4e07924dd4d
CedricJardon <yopla666@gmail.com>
2024-02-09 13:19:18 +0100

    feature(label router datamapper)add files but not ended

--- app/datamappers/label.datamapper.js
+++ app/datamappers/label.datamapper.js
@@ -1,3 +1,18 @@
+// import client from '../helpers/pg.client.js';
+
+/**
+ * @typedef {object} Label
+ * @property {integer} id - Label id
+ * @property {string} name - Label name
+ * @property {integer} year - Label year of creation
+ * @property {string} city - Label city
+ * @property {integer} country - Label country
+ * @property {string} description - Label description
+ * @property {string} url_image - Label url_image
+ * @property {string} created_at - User created_at
+ * @property {string} updated_at - User updated_at
+ */
+
 import CoreDatamapper from './core.datamapper.js';
 
 export default class LabelDatamapper extends CoreDatamapper {
---
+++ app/routers/api/site/label.router.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import labelController from '../../../controllers/label.controller.js';
+import controllerWrapper from '../../../helpers/controller.wrapper.js';
+
+const labelRouter = express.Router();
+
+labelRouter
+  .route('/')
+  /**
+   * GET /api/labels
+   * @summary Get all labels
+   * @tags Labels
+   * @return {User[]} 200 - success response - application/json
+   * @return {ApiJsonError} 400 - Bad request response - application/json
+   * @return {ApiJsonError} 500 - Internal Server Error - application/json
+   */
+  .get(
+    controllerWrapper(labelController.getAll.bind(labelController)),
+  );
+
+labelRouter
+  .route('/:id(\\d+)')
+  /**
+   * GET /api/labels/{id}
+   * @summary Get a label from its id
+   * @tags Labels
+   * @param {number} id.path.required - label id
+   * @return {User} 200 - success response - application/json
+   * @return {ApiJsonError} 400 - Bad request response - application/json
+   * @return {ApiJsonError} 404 - Not found response - application/json
+   * @return {ApiJsonError} 500 - Internal Server Error - application/json
+   */
+  .get(controllerWrapper(labelController.getByPk.bind(labelController)));
+
+/*
+labelRouter
+  .route('/albums')
+  /**
+   * GET /api/labels/albums
+   * @summary Get all albums of all labels
+   * @tags Labels
+   * @return {User[]} 200 - success response - application/json
+   * @return {ApiJsonError} 400 - Bad request response - application/json
+   * @return {ApiJsonError} 500 - Internal Server Error - application/json
+  // remettre ici la fin du bloc swagger
+  .get(
+    controllerWrapper(labelController.getAllAlbum.bind(labelController)),
+  );
+
+labelRouter
+  .route('/:id(\\d+)/albums')
+  /**
+   * GET /api/labels/{id}/albums
+   * @summary Get all albums of one label
+   * @tags Labels
+   * @return {User[]} 200 - success response - application/json
+   * @return {ApiJsonError} 400 - Bad request response - application/json
+   * @return {ApiJsonError} 500 - Internal Server Error - application/json
+  // remettre ici la fin du bloc swagger
+  .get(
+    controllerWrapper(labelController.getAllAlbumOfOneLabel),
+  );
+*/
+export default labelRouter;
