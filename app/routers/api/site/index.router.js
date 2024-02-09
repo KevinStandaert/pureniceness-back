14017e2f8f96090109939fa58eb8ec3a0a029949
CedricJardon <yopla666@gmail.com>
2024-02-09 13:15:17 +0100

    fix(index.router.js)fixing routes

--- app/routers/api/site/index.router.js
+++ app/routers/api/site/index.router.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import ApiError from '../../../errors/api.error.js';
 import apiUserRouter from './user.router.js';
+import apiAuthRouter from './auth.router.js';
+import apiLabelRouter from './label.router.js';
 
 const apiSiteRouter = express.Router();
 
 apiSiteRouter.use('/users', apiUserRouter);
+
+apiSiteRouter.use('/auth', apiAuthRouter);
+
+apiSiteRouter.use('/labels', apiLabelRouter);
 
 apiSiteRouter.use((_, __, next) => {
   next(new ApiError('Resource not found', { httpStatus: 404 }));
