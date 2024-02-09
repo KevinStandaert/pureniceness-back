d7df7db335830746a7a2c433b8db237b6b1b309e
CedricJardon <yopla666@gmail.com>
2024-02-09 13:14:39 +0100

    fix(index.router.js)fxing routes

--- app/routers/api/index.router.js
+++ app/routers/api/index.router.js
@@ -1,16 +1,13 @@
 import express from 'express';
 import apiRouter from './site/index.router.js';
 import adminRouter from './admin/index.router.js';
-import authRouter from './site/auth.router.js';
 import errorHandler from '../../helpers/error.handler.js';
 
 const router = express.Router();
 
-router.use('/site', apiRouter);
+router.use('/', apiRouter);
 
 router.use('/admin', adminRouter);
-
-router.use('/auth', authRouter);
 
 router.use(errorHandler);
 
