b720417973db516dd6a4aa9498f1d1daa53738be
CedricJardon <yopla666@gmail.com>
2024-02-20 16:37:49 +0100

    fix(userliketracks)using of view user_link_with_track

--- migrations/deploy/user_link_with_track.sql
+++ migrations/deploy/user_link_with_track.sql
@@ -4,18 +4,19 @@ BEGIN;
 
 CREATE VIEW "user_link_with_track" AS
 SELECT
-"user"."id" AS "user_id",
-json_agg("track".*) AS "likes"
-FROM "user_like_track"
-JOIN "user"
-ON
-"user_like_track"."user_id" = "user"."id"
-JOIN "track"
-ON
-"user_like_track"."track_id" = "track"."id"
-JOIN "album"
-ON
-"track"."album_id" = "album"."id"
-GROUP BY "user"."id";
+    "user"."id" AS "user_id",
+    "track".*,
+    "album"."name" AS "album_name",
+    "album"."year" AS "album_year",
+    "album"."url_image" AS "url_image_album",
+    "album"."type" AS "album_type"
+FROM
+    "user_like_track"
+JOIN
+    "user" ON "user_like_track"."user_id" = "user"."id"
+JOIN
+    "track" ON "user_like_track"."track_id" = "track"."id"
+JOIN
+    "album" ON "track"."album_id" = "album"."id";
 
 COMMIT;
