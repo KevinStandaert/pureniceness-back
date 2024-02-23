12f30eace7e855a3bdc155a4b1b6b9a2b5aeb0cb
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-23 22:05:33 +0100

    fix(track_with_artist): fix view

--- migrations/deploy/track_with_artist.sql
+++ migrations/deploy/track_with_artist.sql
@@ -3,16 +3,28 @@
 BEGIN;
 
 CREATE VIEW "track_with_artist" AS
-SELECT 
-"track".*,
-json_agg("artist".*) AS "artist"
-FROM "track_has_artist"
-JOIN "track"
-ON
-"track_has_artist"."track_id" = "track"."id"
-JOIN "artist"
-ON
-"track_has_artist"."artist_id" = "artist"."id"
-GROUP BY "track"."id";
+ SELECT 
+    "track".*,
+    CASE 
+        WHEN "track_has_artist"."track_id" IS NULL THEN '[]'::json
+        ELSE json_agg(json_build_object(
+            'id', "artist"."id",
+            'firstname', "artist"."firstname",
+            'lastname', "artist"."lastname",
+            'nickname', "artist"."nickname",
+            'year', "artist"."year",
+            'city', "artist"."city",
+            'country', "artist"."country",
+            'description', "artist"."description",
+            'function', "artist"."function",
+            'url_image', "artist"."url_image",
+            'role', "track_has_artist"."role",
+            'order', "track_has_artist"."order"
+        ))
+    END AS "artists"
+FROM "track"
+LEFT JOIN "track_has_artist" ON "track"."id" = "track_has_artist"."track_id"
+LEFT JOIN "artist" ON "track_has_artist"."artist_id" = "artist"."id"
+GROUP BY "track"."id",  "track_has_artist"."track_id";
 
 COMMIT;
