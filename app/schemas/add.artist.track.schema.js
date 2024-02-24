dd2720ce5eead69d1099f02442930e239689af24
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-24 22:39:40 +0100

    feat(artistAddSchema): add order to schema

--- app/schemas/add.artist.track.schema.js
+++ app/schemas/add.artist.track.schema.js
@@ -5,6 +5,8 @@ const artistAddSchema = Joi.object({
     .error(new Error('Le role de l\'artiste est requis et doit être une chaîne de caractères')),
   artist_id: Joi.number().integer().positive().required()
     .error(new Error('L\'identifiant e l\'artiste est requis et doit être un entier positif')),
+  order: Joi.number().integer().min(0).required()
+    .error(new Error('L\'index de l\'ordre doit être supérieur ou égal à 0')),
 });
 
 export default artistAddSchema;
