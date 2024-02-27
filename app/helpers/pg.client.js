1c0645cedb9f586e38a02cb1c2ca6fa55a309c66
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-27 13:11:03 +0100

    build(pg.client): add PGPASSWORD to params of client connect

--- app/helpers/pg.client.js
+++ app/helpers/pg.client.js
@@ -4,6 +4,7 @@ import pg from 'pg';
 const client = new pg.Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
 });
 // There is no direct connection with a client when using a pool,
