f843bb6ee4988e1738b10fa32aed66feefdc85cc
SAURETMathieu <mathieu.sauret@oclock.school>
2024-03-04 13:10:32 +0100

    feat(core.datamapper): add ordered function

--- app/datamappers/core.datamapper.js
+++ app/datamappers/core.datamapper.js
@@ -7,6 +7,8 @@ export default class CoreDatamapper {
 
   static writeTableName;
 
+  static orderedTableName;
+
   static async findAll() {
     const result = await client.query(`SELECT * FROM "${this.readTableName}"`);
     const resultWithoutPassword = result.rows.map(({ password, ...rest }) => rest);
@@ -46,6 +48,38 @@ export default class CoreDatamapper {
 
   static async delete(id) {
     const result = await client.query(`DELETE FROM "${this.writeTableName}" WHERE "id" = $1`, [id]);
+    return !!result.rowCount;
+  }
+
+  static async ordered(id, orderList) {
+    // Initialize the SQL update query
+    let updateQuery = `UPDATE "${this.orderedTableName}" SET "order" = CASE `;
+
+    // Initialize arrays to hold placeholders and values for the query
+    const values = [];
+
+    // Iterate over the order list to construct placeholders and values
+    orderList.forEach((element, index) => {
+      values.push(element.id, index);
+    });
+
+    // Iterate over the order list to construct the "WHEN" clauses for the SQL update query
+    orderList.forEach((elementId, index) => {
+      const whenIndex = index * 2 + 1;
+      updateQuery += `WHEN "id" = $${whenIndex} THEN $${whenIndex + 1} `;
+    });
+
+    // Add the "ELSE" and "END" parts of the SQL update query
+    updateQuery += 'ELSE "order" END ';
+
+    // Construct the "WHERE" clause for the SQL update query
+    updateQuery += `WHERE "${this.writeTableName}_id" = $${orderList.length * 2 + 1}`;
+
+    // Add the albumId to the values array
+    values.push(parseInt(id, 10));
+    const result = await client.query(updateQuery, values);
+
+    // Return true if the update was successful, otherwise false
     return !!result.rowCount;
   }
 }
