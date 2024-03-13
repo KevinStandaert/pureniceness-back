d6adb49cb12715d2443b6952b93bbb4e13c53db3
MerylCordier <meryl.cordier@oclock.school>
2024-03-13 15:18:04 +0100

    deploy(pg): change url for db connection

--- app/helpers/pg.client.js
+++ app/helpers/pg.client.js
@@ -2,10 +2,14 @@ import pg from 'pg';
 
 // The Pool allows opening multiple connections that can be used in parallel.
 const client = new pg.Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  // host: process.env.PGHOST,
+  // user: process.env.PGUSER,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 // There is no direct connection with a client when using a pool,
 // as the pool manages the opening and closing of connections.
